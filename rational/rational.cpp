deb3210e24548c540b1943c52dbcdc6e4413050d
Judson Steward <jsteward2@harding.edu>
2025-10-05 21:18:35 -0500

    Add project files.

---
+++ rational/rational.cpp
@@ -0,0 +1,12 @@
+﻿// rational.cpp : Defines the entry point for the application.
+//
+
+#include "rational.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
